f38d9c11172762b9d09084dce19a15b136afc0bd
Ahmad Alhineidi <ahmad.alhineidi@unibe.ch>
2025-11-14 10:49:28 +0100

    prepare taks for 2025

--- task_3.R
+++ task_3.R
@@ -17,12 +17,8 @@ multiply_vectors <- function(v1, v2) {
 
 # Task 1: Implement a function to subtract two vectors, similar to the existing functions above.
 # Subtract two vectors
-subtract_vectors <- function(v1, v2){
-  return(v1 - v2)
-}
+
 
 
 # Task 2: Implement a function to divide two vectors, similar to the existing functions above.
-divide_vectors <- function(v1, v2){
-  return(v1/v2)
-}
+
